0f23ca68f65a12b64ddb4025ef48920503ecfcc6
Giovani Stein <giovanistein10@gmail.com>
2022-06-11 16:13:56 -0300

    first commit

---
+++ src/main/java/com/example/authapi/repository/UsuarioRepository.java
@@ -0,0 +1,12 @@
+package com.example.authapi.repository;
+
+import com.example.authapi.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+    Optional<Usuario> findByEmail(String email);
+
+}
